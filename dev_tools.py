1f093dec470a274ab12039f35bb7f4b77fb470fe
NII <patrick@niip.me>
2024-06-03 22:14:59 +0200

    amelioration et optimisation du code

--- dev_tools.py
+++ dev_tools.py
@@ -5,26 +5,23 @@ import time
 from skimpy import skim
 from datetime import datetime
 
-# Fonction pour récupérer les données à partir de la bas de données
-def keep_data(data):
-    
+# Initialisation des DataFrames globaux
+logs_vols = pd.DataFrame()
+df_degrade = pd.DataFrame()
+
+# Fonction pour récupérer les données à partir de la base de données
+def keep_data():
     today = datetime.today().strftime('%Y-%m-%d')
     
-    logs_url = f"http://sc-e.fr/docs/logs_vols_{today}.csv"
-    degradations_url = f"http://sc-e.fr/docs/degradations_{today}.csv"
+    logs_data = pd.read_csv(f"http://sc-e.fr/docs/logs_vols_{today}.csv", sep=",")
+    degradations_data = pd.read_csv(f"http://sc-e.fr/docs/degradations_{today}.csv", sep=",")
     
-    logs_data = pd.read_csv(logs_url)
-    degradations_data = pd.read_csv(degradations_url)
-    
-    global logs_vols, df_degrade 
-    
-    logs_vols = pd.concat([logs_data, logs_vols], axis=0)
-    df_degrade = pd.concat([degradations_data, degradations], axis=0)
-
+    logs_vols = pd.concat([logs_vols, logs_data], axis=0)
+    df_degrade = pd.concat([df_degrade, degradations_data], axis=0)
 
 # Fonction de nettoyage
-def autoclean_dataset(data):
-     # Exploration des données
+def autoclean_dataset():
+    data = logs_vols  # Ou df_degrade selon ce que vous voulez nettoyer
     print("Résumé des données :")
     print(skim(data))
     print("Valeurs manquantes :")
@@ -34,12 +31,14 @@ def autoclean_dataset(data):
     
     # Copie du dataset original pour les modifications
     cleaned_data = data.copy()
-   
 
 # Définir la planification des tâches
-schedule.every().day.at("09:30").do(keep_data).do(autoclean_dataset)
+schedule.every().day.at("09:30").do(keep_data)
+schedule.every().day.at("09:31").do(autoclean_dataset)
 
+end_date = datetime(2024, 7, 27)
 # Boucle pour exécuter les tâches planifiées
-while True:
+while datetime.now() < end_date:
     schedule.run_pending()
     time.sleep(1)
+print("Fin du programme")
