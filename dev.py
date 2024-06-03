42be74c27bfa8d46a5492ebaadac6f2cd77ebf6d
NII <patrick.nii@aol.com>
2024-06-03 18:56:25 +0200

    suppresion de code inutile pour l'algo de traitement de données automatique

--- dev.py
+++ dev.py
@@ -14,8 +14,8 @@ def hello():
     print("hello, how are you?")
 
 # Définir les tâches pour qu'elles se lancent toutes les 10 secondes
-schedule.every(10).seconds.do(current_time)
-schedule.every(10).seconds.do(hello)
+schedule.every(1).days.do(current_time)
+schedule.every(1).days.do(hello)
 
 # Lancer la boucle pour exécuter les tâches planifiées
 while True:
@@ -49,10 +49,10 @@ def autoclean_dataset(dataset_path):
         # Chargement du dataset
         data = pd.read_csv(dataset_path)
     except FileNotFoundError:
-        print("Le fichier spécifié n'existe pas.")
+        print("Le fichier n'existe pas.")
         return None
-    except Exception as e:
-        print("Une erreur s'est produite lors du chargement du dataset :", e)
+    except Exception:
+        print("Erreur lors du chargement du dataset :")
         return None
     
     # Suppression des doublons
@@ -60,47 +60,28 @@ def autoclean_dataset(dataset_path):
     data.drop_duplicates(inplace=True)
     duplicates_removed = original_shape[0] - data.shape[0]
     if duplicates_removed > 0:
-        print(f"\n{duplicates_removed} lignes en double ont été supprimées.")
+        print(f"\n{duplicates_removed} Doublons supprimées.")
     else:
-        print("\nAucune ligne en double n'a été trouvée.")
+        print("Aucun doublons trouvée.")
         
         
     # Exploration des données
-    print("Informations sur le dataset :")
-    print(data.info())
-    print("\nRésumé des données :")
+    print("Résumé des données :")
     print(skim(data))
-    print("\nValeurs manquantes :")
+    print("Valeurs manquantes :")
     print(data.isnull().sum())
-    print("\nValeurs uniques par colonne :")
+    print("Valeurs uniques par colonne :")
     print(data.nunique())
     
     # Copie du dataset original pour les modifications
     cleaned_data = data.copy()
-    
-    '''Identifiez et résolvez les problèmes potentiels
-    (Exemples: mise en unité, normalisation des codes, détection des valeurs aberrantes)
-    Standardisez le processus (Exemple: automatiser le nettoyage)
-    
-    Visualisation des données (Exemples: histogrammes, nuages de points, etc.)
-    Effectuez une vérification orthographique (si applicable)
-    Nettoyez les caractères non reconnus
-    
-    Uniformisation des données temporelles
-    Uniformisation des données chiffrées
-    
-    Parsing (utilisation des expressions régulières pour extraire des informations)
-    Transformation de données (par ex. extraction d'années à partir de dates)
-    
-    Renforcement des contraintes d'intégrité
-    Méthode statistique (ex: détection et correction d'erreurs avec des méthodes statistiques)'''
     
     # Sauvegarde du dataset nettoyé
     output_folder = "df_clean"
     os.makedirs(output_folder, exist_ok=True)
     output_path = os.path.join(output_folder, "df_clean.csv")
     data.to_csv(output_path, index=False)
-    print(f"\nDataset nettoyé sauvegardé sous : {output_path}")
+    print(f"Dataset nettoyé sauvegardé sous : {output_path}")
     
     # Retourner le dataset nettoyé
     return data
