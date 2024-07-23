f513b85a4e93dd85bf9210d7f669be6ef36ccbb9
Patrick-NII <patrick.nii@aol.com>
2024-07-23 15:59:19 +0200

    test 2

--- Algo_Automation/script.py
+++ Algo_Automation/script.py
@@ -146,14 +146,18 @@
 # print("Fin du programme")
 
 
-
 import os
 import json
 import pandas as pd
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
+import warnings
+
+# Ignorer les FutureWarnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 # Configuration de la connexion à la base de données MySQL
 db_user = 'Top_gun'
@@ -163,13 +167,12 @@ db_port = '3306'
 db_name = 'Top_gun'
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-def load_data_url():
-    today = datetime.today().strftime('%Y-%m-%d')
-    degrade_url = f"http://sc-e.fr/docs/degradations_{today}.csv"
+def load_data_url(date):
+    degrade_url = f"http://sc-e.fr/docs/degradations_{date}.csv"
     
     try:
         df_degrade = pd.read_csv(degrade_url)
-        print(f"Données chargées depuis {degrade_url} pour la date {today}")
+        print(f"Données chargées depuis {degrade_url} pour la date {date}")
     except Exception as e:
         print(f"Erreur lors du chargement des données depuis {degrade_url} : {e}")
         df_degrade = pd.DataFrame()
@@ -191,17 +194,30 @@ def save_to_database(df, table_name, engine):
         print(f"Erreur lors de l'insertion des données dans la table {table_name} : {e}")
 
 def main():
-    # Téléchargement des nouvelles données de dégradations depuis l'URL
-    df_degrade = load_data_url()
+    # Date de début historique
+    start_date = datetime(2024, 6, 2).date()
+    # Date du jour
+    today = datetime.today().date()
     
-    # Nettoyage des données de dégradations
-    cleaned_df_degrade = autoclean_degradations(df_degrade)
-    
-    # Enregistrement des données nettoyées dans la base de données
-    if not cleaned_df_degrade.empty:
-        save_to_database(cleaned_df_degrade, 'degradations', engine)
-    else:
-        print("Aucune donnée de dégradations à ajouter à la base de données.")
+    # Itérer sur chaque date de la plage de temps
+    current_date = start_date
+    while current_date <= today:
+        date_str = current_date.strftime('%Y-%m-%d')
+        
+        # Téléchargement des nouvelles données de dégradations depuis l'URL
+        df_degrade = load_data_url(date_str)
+        
+        # Nettoyage des données de dégradations
+        if not df_degrade.empty:
+            cleaned_df_degrade = autoclean_degradations(df_degrade)
+            
+            # Enregistrement des données nettoyées dans la base de données
+            save_to_database(cleaned_df_degrade, 'degradations', engine)
+        else:
+            print(f"Aucune donnée de dégradations à ajouter pour la date {date_str}.")
+        
+        # Passer à la date suivante
+        current_date += timedelta(days=1)
 
 if __name__ == "__main__":
     main()
