4282e66fad6ef8ca2ccda4dc90f53f999c0b1258
Patrick-NII <patrick.nii@aol.com>
2024-07-25 15:12:37 +0200

    add

--- push_to_all.sh
+++ push_to_all.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 remotes=("myrepo" "collegerepo")
 
@@ -6,4 +7,13 @@ branch="algo/data-etl"
 for remote in "${remotes[@]}";do
 	echo "Pushing to $remote"
 	git push $remote $branch
+=======
+remotes=("myrepo" "andrearepo")
+
+branch="Patrick"
+
+for remote in "${remotes[@]}"; do
+    echo "Pushing to $remote"
+    git push $remote $branch
+>>>>>>> c696780c62bbe1d292208a62dd625bd238ace301
 done
